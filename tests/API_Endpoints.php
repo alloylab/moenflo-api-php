870e6fbb8de2ee2bedcb329f615270c1a4bba3ae
Trevor Ferre <trevor@alloylab.com>
2021-02-01 14:13:23 -0600

    ci var test 2

--- tests/API_Endpoints.php
+++ tests/API_Endpoints.php
@@ -18,12 +18,14 @@ try {
     $location_id = getenv('moen_location_id');
     $mac_address = getenv('moen_mac_address');
 
-    $Moen_API = new \MoenFlo\API($username, $password, $moen_user_id);
+    echo $location_id;
 
-    $Moen_API->set_device_settings($device_id);
-    $Moen_API->alert_override($device_id);
-    $Moen_API->valve_unit_status($device_id, 'open');
-    $Moen_API->device_consumption($location_id, $mac_address, '12/1/2020', '12/31/2020');
+    //$Moen_API = new \MoenFlo\API($username, $password, $moen_user_id);
+
+    //$Moen_API->set_device_settings($device_id);
+    //$Moen_API->alert_override($device_id);
+    //$Moen_API->valve_unit_status($device_id, 'open');
+    //$Moen_API->device_consumption($location_id, $mac_address, '12/1/2020', '12/31/2020');
 } catch (Exception $e) {
     throw new Exception($e);
 }
